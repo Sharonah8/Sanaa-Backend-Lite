336773be517a80c3c34c6e174aea5e167c05e77a
Ian Mike <ian.kaibi@student.moringaschool.com>
2023-01-11 14:46:34 +0300

    Add rspec and regenerate models and controllers with respective test files

--- db/migrate/20230111113845_create_reviews.rb
+++ db/migrate/20230111113845_create_reviews.rb
@@ -1,6 +1,6 @@
-class CreateFeedbacks < ActiveRecord::Migration[7.0]
+class CreateReviews < ActiveRecord::Migration[7.0]
   def change
-    create_table :feedbacks do |t|
+    create_table :reviews do |t|
       t.string :comment
       t.string :name
       t.string :image_url
