2f6b9f5188b25fe356f9fcf5950ffc6c6256e175
Ian Mike <ian.kaibi@student.moringaschool.com>
2023-01-12 12:26:19 +0300

    Add actions to the art_pieces controller

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -1,2 +1,38 @@
 class ArtPiecesController < ApplicationController
+  def index
+    artpieces = ArtPiece.all
+    render json: artpieces
+  end
+
+  def show
+    artpiece = find_artpiece
+    render json: artpiece
+  end
+
+  def create
+    artpiece = ArtPiece.create!(artpieces_params, recruiter_id: 1)
+    render json: artpiece, status: :created
+  end
+
+  def update
+    artpiece = find_artpiece
+    artpiece.update!(art_pieces_params)
+    render json: artpiece
+  end
+
+  def destroy
+    artpiece = find_artpiece
+    artpiece.destroy
+    head :no_content, status: :no_content
+  end
+
+  private
+
+  def art_pieces_params
+    params.permit(:image_url, :title, :description)
+  end
+
+  def find_artpiece
+    ArtPiece.find(params[:id])
+  end
 end
