15deb5d4fa9f2f8c52a946a204f5793bb88ee6f5
mulukimwangeka <mwangekaruth@gmail.com>
2023-01-12 16:29:38 +0300

    relationships and validations added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,14 @@
 class Review < ApplicationRecord
+    belongs_to :user
+    belongs_to :art_piece
+
+    validates :comment, presence: true
+    validates :user, presence: true
+    validates :art_piece, presence: true
+    validates :user, uniqueness: { scope: :art_piece_id }
+    validates :art_piece, uniqueness: { scope: :user_id }
+    
+
+    
 end
+
