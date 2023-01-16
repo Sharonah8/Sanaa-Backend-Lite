b054693f7d772c85ab0bb88172968484d99db515
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2023-01-16 12:52:34 +0300

    Modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,12 @@ puts "🌱 Seeding begins here...."
 puts "Deleting old data..."
 Review.destroy_all
 User.destroy_all
-Art.destroy_all
+ArtPiece.destroy_all
 
 # Seed your database here
 #make 5 users
 puts "Creating users..."
-user1 = User.create(name: Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password)
-user2 = User.create(name: Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password)
-user3 = User.create(name: Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password)
-user4 = User.create(name: Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password)
-user5 = User.create(name: Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password)
+user1 = User.create(username: Faker::Name.name, email:"sanaa_art@gmail.com", password:"sanaa")
 
 # 5.times do |i|
 #     User.create(name:Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password)
@@ -28,18 +24,18 @@ user5 = User.create(name: Faker::Name.name, email:Faker::Internet.email, passwor
 
 #make 5 Arts
 puts "Creating Arts..."
-arts1 = Art.create(image_url: "", title: "The Awakening", description: "The Awakening inspired by", price: Faker::Commerce.price)
-arts2 = Art.create(image_url: "", title: "The Potrait", description: "The Potrait inspired by", price: Faker::Commerce.price)
-arts3 = Art.create(image_url: "", title: "The Mystery", description: "The Mystery inspired by", price: Faker::Commerce.price)
-arts4 = Art.create(image_url: "", title: "The Culture", description: "The Culture inspired by The African continent.", price: Faker::Commerce.price)
-arts5 = Art.create(image_url: "", title: "The Guide", description: "The Guide inspired by", price: Faker::Commerce.price)
+arts1 = ArtPiece.create(image_url: "", title: "The Awakening", description: "The Awakening inspired by", price: Faker::Commerce.price)
+arts2 = ArtPiece.create(image_url: "", title: "The Potrait", description: "The Potrait inspired by", price: Faker::Commerce.price)
+arts3 = ArtPiece.create(image_url: "", title: "The Mystery", description: "The Mystery inspired by", price: Faker::Commerce.price)
+arts4 = ArtPiece.create(image_url: "", title: "The Culture", description: "The Culture inspired by The African continent.", price: Faker::Commerce.price)
+arts5 = ArtPiece.create(image_url: "", title: "The Guide", description: "The Guide inspired by", price: Faker::Commerce.price)
 
 puts "Creating Review..."
-reviews1 = Review.create(comment: "Great Inspiration.", name: Faker::Name.name, photo_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
-reviews2 = Review.create(comment: "My Favourite!", name: Faker::Name.name, photo_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
-reviews3 = Review.create(comment: "So much going behind this.", name: Faker::Name.name, photo_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
-reviews4 = Review.create(comment: "Take me back to the roots.", name: Faker::Name.name, photo_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
-reviews5 = Review.create(comment: "This is creative.", name: Faker::Name.name, photo_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
+reviews1 = Review.create(comment: "Great Inspiration.", name: Faker::Name.name, image_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
+reviews2 = Review.create(comment: "My Favourite!", name: Faker::Name.name, image_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
+reviews3 = Review.create(comment: "So much going behind this.", name: Faker::Name.name, image_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
+reviews4 = Review.create(comment: "Take me back to the roots.", name: Faker::Name.name, image_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
+reviews5 = Review.create(comment: "This is creative.", name: Faker::Name.name, image_url: "https://i.pinimg.com/236x/87/42/31/8742315158e5de302ae9c5a2f9bee233.jpg")
 
 puts "✅ Done seeding!"
 
