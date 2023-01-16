fbdba5d32319f55e9656f500ee551468f43c6df3
mulukimwangeka <mwangekaruth@gmail.com>
2023-01-16 15:02:24 +0300

    Modify model validations

--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -1,5 +1,5 @@
 class ArtPiece < ApplicationRecord
     has_many :reviews
-    has_many :users, through: :reviews
+    # has_many :users, through: :reviews
 end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,13 +2,9 @@ class Review < ApplicationRecord
     belongs_to :user
     belongs_to :art_piece
 
-    validates :comment, presence: true
-    validates :user, presence: true
-    validates :art_piece, presence: true
-    validates :comment, length: { minimum: 12 }
-
-
-
+    validates :comment, :name, :image_url, presence: true
+    validates :name,  length: {maximum: 10}
+    validates :comment, length: { maximum: 12 }
     
 end
 
