7a50a2d73a0cc722e278681cf7f18bf2cef79350
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2023-01-16 12:43:24 +0300

    Modify a user model column and regenerate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_114205) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_091055) do
   create_table "art_pieces", force: :cascade do |t|
     t.string "image_url"
     t.string "title"
@@ -33,9 +33,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_114205) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
