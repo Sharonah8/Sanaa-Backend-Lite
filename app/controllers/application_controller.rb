e412ad1fae3b4a73c7a9c3005b1a241b5b6f2e6c
erickmuriithi <rickymurithii@gmail.com>
2023-01-18 11:42:00 +0300

    fix authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,12 @@ class ApplicationController < ActionController::API
   before_action :authenticate_request
 
   private
-
+  
   def authenticate_request
     header = request.headers["Authorization"]
     header = header.split(" ").last if header
     decoded = jwt_decode(header)
-    @current_user = User.find(decoded[:user_id])
+    @current_user = decoded ? User.find(decoded[:user_id]) : nil
+    render json: { message: 'Please sign in' }, status: :unauthorized unless decoded
   end
 end
--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -8,9 +8,13 @@ module JsonWebToken
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
-
+  
   def jwt_decode(token)
+    begin
     decoded = JWT.decode(token, SECRET_KEY)[0]
     HashWithIndifferentAccess.new decoded
+    rescue 
+      nil
+    end
   end
 end
